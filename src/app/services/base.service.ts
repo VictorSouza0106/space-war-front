f75f49359d71f49021dbc0fbfe93bcc62ab16584
Victor Souza <victorsouza0106@gmail.com>
2025-05-12 22:06:52 -0300

    .

--- src/app/services/base.service.ts
+++ src/app/services/base.service.ts
@@ -8,11 +8,11 @@ export class BaseService {
   protected http = inject(HttpClient);
 
   // public BASE_REF = 'http://localhost:3000';
-  public BASE_REF = 'https://tabulabs-344fkcab8f850.herokuapp.com';
+  public BASE_REF = 'https://tabulabs-344fcab8f850.herokuapp.com';
 
   getRequestOptions() {
     return {
-      withCredentials: true,
+      withCredentials: false,
       headers: this.getRequestHeaders(),
     };
   }
